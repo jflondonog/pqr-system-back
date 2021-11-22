75ba7bd9a5446a15b1dacb1acf5a6112f0000865
jflondonog <jflondonog@eafit.edu.co>
2021-11-22 10:55:35 -0500

    Deploy Backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const conectarBD = require('./config/db');
+const cors = require("cors")
+const path = require("path")
+const dotenv = require("dotenv").config();
+
+//SERVIDOR
+const app = express();
+
+conectarBD();
+app.use(cors())
+app.use(express.json());
+
+app.use('/api/solicitudes', require('./routes/solicitud'));
+
+
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "cliente", "build", "src/index.html"));
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () =>{
+    console.log('El servidor está corriendo')
+})
